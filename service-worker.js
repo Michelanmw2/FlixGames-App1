60cdf29699fc32ea24c2d9af894c3298bc6adbe4
Michelanmw2 <142499970+Michelanmw2@users.noreply.github.com>
2026-06-25 19:10:04 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,16 @@
-self.addEventListener("install", function(event) {
+const CACHE_NAME = 'flixgames-app-v1';
+const FILES_TO_CACHE = ['/', '/index.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/splash.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
